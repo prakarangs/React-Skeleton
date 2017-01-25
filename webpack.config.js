70beecb9a9a2cf85af5d31b5b5f38fb9033bf5a3
kao <kao@aklamio.com>
2017-01-25 12:37:44 +0100

    Add basic setup

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  // Entry accepts a path or an object of entries.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  }
+}
